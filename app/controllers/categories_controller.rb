63018c8b2c203310d8aa21980a7777ae52f7f5b5
Kaizilege Karoma <kkaroma@gmail.com>
2022-05-31 19:05:48 +0300

    Add delete method

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,6 +2,7 @@ class CategoriesController < ApplicationController
   before_action :set_category, only: %i[ show edit update destroy ]
   before_action :authenticate_user!
 
+
   # GET /categories or /categories.json
   def index
     @categories = Category.all
@@ -22,17 +23,17 @@ class CategoriesController < ApplicationController
 
   # POST /categories or /categories.json
   def create
-    @category = Category.new(category_params)
+      @category = Category.new(category_params)
 
-    respond_to do |format|
-      if @category.save
-        format.html { redirect_to category_url(@category), notice: "Category was successfully created." }
-        format.json { render :show, status: :created, location: @category }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @category.errors, status: :unprocessable_entity }
+      respond_to do |format|
+        if @category.save
+          format.html { redirect_to category_url(@category), notice: "Category was successfully created." }
+          format.json { render :show, status: :created, location: @category }
+        else
+          format.html { render :new, status: :unprocessable_entity }
+          format.json { render json: @category.errors, status: :unprocessable_entity }
+        end
       end
-    end
   end
 
   # PATCH/PUT /categories/1 or /categories/1.json
--- app/helpers/categories_helper.rb
+++ app/helpers/categories_helper.rb
@@ -1,2 +1,10 @@
 module CategoriesHelper
+    def user_admin()
+       # if user.role == 'admin'
+       if current_user
+        if current_user.role == 'admin'
+            return true
+        end
+       end
+    end
 end
