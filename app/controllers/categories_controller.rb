217afe5087fe4b63e3b9261dcd43884a3beb5a81
Kaizilege Karoma <kkaroma@gmail.com>
2023-08-04 19:50:46 +0300

    working on the menu

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -60,9 +60,14 @@ class CategoriesController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
+
+        # Use callbacks to share common setup or constraints between actions.
     def set_category
       @category = Category.find(params[:id])
+    end
+
+    def admin_user
+
     end
 
     # Only allow a list of trusted parameters through.
