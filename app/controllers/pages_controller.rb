60ac8ed3b0c088e49b0219c093deca373feed076
Kaizilege Karoma <kkaroma@gmail.com>
2023-08-13 13:31:00 +0300

    Fixing the main menu

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,8 @@
 class PagesController < ApplicationController
     def index
-        @courses = Course.where(user_id: 1).order("id DESC")
+        #@courses = Course.where(user_id: 1).order("id DESC")
+        #@courses = Course.includes(:user).order("id DESC")
+        @courses = Course.all.where(user_id: 2)
        
     end
 end
