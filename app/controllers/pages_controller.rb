7890231ecc201123c793b4cf77791f6d0a8a0da1
Kaizilege Karoma <kkaroma@gmail.com>
2023-08-16 22:01:51 +0300

    Allow course list on index page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,7 @@ class PagesController < ApplicationController
     def index
         #@courses = Course.where(user_id: 1).order("id DESC")
         #@courses = Course.includes(:user).order("id DESC")
-        @courses = Course.all.where(user_id: 1)
+        @courses = Course.all.where(user_id: 2)
        
     end
 end
