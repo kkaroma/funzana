f3da642dbba2714ae8d5a8e1b914ea10716b9edb
Kaizilege Karoma <kkaroma@gmail.com>
2023-08-16 21:12:12 +0300

    Force rails_admin to login before access

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -1,13 +1,14 @@
 RailsAdmin.config do |config|
   config.asset_source = :sprockets
 
+  
   ### Popular gems integration
 
   ## == Devise ==
-  # config.authenticate_with do
-  #   warden.authenticate! scope: :user
-  # end
-  # config.current_user_method(&:current_user)
+  config.authenticate_with do
+    warden.authenticate! scope: :user
+  end
+   config.current_user_method(&:current_user)
 
   ## == CancanCan ==
   # config.authorize_with :cancancan
